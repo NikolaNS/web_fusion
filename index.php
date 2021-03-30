b301fa8555533baf01cbd165c8f803d2433e2f21
NikolaNS <nikola.radun@gmail.com>
2021-03-30 15:39:00 +0200

    initial comit

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <title>Homepage</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 <body>
    
@@ -14,8 +16,7 @@
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="">Prices</a></li>
-            <li><a href="">Packages</a></li>
-            <li><a href="">Blog</a></li>
+            <li><a href="">Packages</a></li> 
             <li><a href="">Contact</a></li>
         </ul>
     </nav>
@@ -29,6 +30,52 @@
             <input type="submit" value="Contact">
         </section>
     </header>
+    
+<!--    portfolio-->
+  
+  <section class="container portfolio">
+      <article>
+          <h2>New projects</h2>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum iure magnam error aliquam, <br> dicta mollitia quod temporibus minima eos porro, dignissimos aspernatur perspiciatis, rem explicabo.</p> 
+      </article>
+      <article> 
+          <button class="button">All projects</button>
+          <button class="button">HTML, CSS, JS</button>
+          <button class="button">Wordpress themes</button>
+          <button class="button">Web shops</button>
+      </article>
+<!--
+      <article>
+          <img src="img/filtr1.jpg" alt="">
+          <img src="img/filtr2.jpg" alt="">
+          <img src="img/filtr3.jpg" alt="">
+          <img src="img/filtr4.jpg" alt="">
+          <img src="img/filtr5.jpg" alt="">
+          <img src="img/filtr6.jpg" alt="">
+      </article>
+-->
+      <div class="photos">
+          <article>
+              <img src="img/filtr1.jpg" alt="" class="color">
+          </article>
+           <article>
+              <img src="img/filtr2.jpg" alt="" class="color">
+          </article>
+          <article>
+              <img src="img/filtr3.jpg" alt="" class="color">
+          </article>
+          <article>
+              <img src="img/filtr4.jpg" alt="" class="color">
+          </article>
+          <article>
+              <img src="img/filtr5.jpg" alt="" class="color">
+          </article>
+          <article>
+              <img src="img/filtr6.jpg" alt="" class="color">
+          </article>
+      </div>
+  </section>
   
 </body>
 </html>
